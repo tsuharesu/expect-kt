93a1759ce0c1d8a93cdc55dec3331fb04a522043
Tsuharesu <tsuharesu01@gmail.com>
2018-09-19 23:50:18 -0300

    Improve generic error strings

--- src/test/kotlin/com/tsuharesu/expectations/GenericExpectationTests.kt
+++ src/test/kotlin/com/tsuharesu/expectations/GenericExpectationTests.kt
@@ -9,6 +9,8 @@ package com.tsuharesu.expectations
 import org.junit.Test
 import kotlin.test.assertFailsWith
 
+private class SomeTestClass
+
 class GenericExpectationTests {
     @Test
     fun toBe() {
@@ -16,23 +18,28 @@ class GenericExpectationTests {
 
         assertFailsWith(AssertionError::class) {
             expect("pass").toBe("fail")
-        }.message.should.be("expected <pass> to be <fail>")
+        }.message.should.be("pass expected to be fail")
 
         "james".should.be("james")
 
         assertFailsWith(AssertionError::class) {
             "pass".should.be("fail")
-        }
+        }.message.should.be("pass expected to be fail")
     }
-
-    class SomeTestClass
 
     @Test
     fun toBeInstanceOf() {
         expect("james").toBeInstanceOf(String::class)
         expect(SomeTestClass()).toBeInstanceOf(SomeTestClass::class)
 
+        assertFailsWith(AssertionError::class) {
+            expect(1).toBeInstanceOf(String::class)
+        }.message.should.be("1 expected to be instance of String")
+
         "james".should.beInstanceOf(String::class)
+        assertFailsWith(AssertionError::class) {
+            "james".should.beInstanceOf(Int::class)
+        }.message.should.be("james expected to be instance of int")
     }
 
     @Test
@@ -41,13 +48,13 @@ class GenericExpectationTests {
 
         assertFailsWith(AssertionError::class) {
             expect("fail").toNotBe("fail")
-        }
+        }.message.should.be("fail expected to not be fail")
 
         "james".should.notBe("fail")
 
         assertFailsWith(AssertionError::class) {
             "fail".should.notBe("fail")
-        }
+        }.message.should.be("fail expected to not be fail")
     }
 
     @Test
@@ -56,14 +63,14 @@ class GenericExpectationTests {
 
         assertFailsWith(AssertionError::class) {
             expect("pass").toBeNull()
-        }
+        }.message.should.be("pass expected to be null")
 
         val x: Int? = null
         x.should.beNull()
 
         assertFailsWith(AssertionError::class) {
             "pass".should.beNull()
-        }
+        }.message.should.be("pass expected to be null")
     }
 
     @Test
@@ -72,13 +79,13 @@ class GenericExpectationTests {
 
         assertFailsWith(AssertionError::class) {
             expect(null).toNotBeNull()
-        }
+        }.message.should.be("expected to not be null")
 
         "asdasd".should.notBeNull()
 
         assertFailsWith(AssertionError::class) {
             val x: Int? = null
             x.should.notBeNull()
-        }
+        }.message.should.be("expected to not be null")
     }
 }
